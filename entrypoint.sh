a317606a7e1a9911dd41141b211072a3fcd4bd89
Huy Tran <tranbahuy@gmail.com>
2021-01-28 10:14:29 +0100

    Fix source

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sed -i 's|##GRAPHDB##|'${GRAPHDB}'|g' /usr/src/app/dn-backend/dn/views.py
-sed -i 's|#DATAVERSE#|'${DATAVERSE}'|g' /usr/src/app/dn-backend/dn/views.py
-sed -i 's|#DATAVERSEEX#|'${DATAVERSEEX}'|g' /usr/src/app/dn-backend/dn/views.py
-sed -i 's|#DATAVERSEKEY#|'${DATAVERSEKEY}'|g' /usr/src/app/dn-backend/dn/views.py
-
-
+sed -i 's|##GRAPHDB##|'${GRAPHDB}'|g' /usr/src/app/dn-backend/dn/utils.py
+sed -i 's|#GRAPHDBUSER#|'${GRAPHDBUSER}'|g' /usr/src/app/dn-backend/dn/utils.py
+sed -i 's|#GRAPHDBPWD#|'${GRAPHDBPWD}'|g' /usr/src/app/dn-backend/dn/utils.py
+sed -i 's|#DATAVERSE#|'${DATAVERSE}'|g' /usr/src/app/dn-backend/dn/utils.py
+sed -i 's|#DATAVERSEEX#|'${DATAVERSEEX}'|g' /usr/src/app/dn-backend/dn/utils.py
+sed -i 's|#DATAVERSEKEY#|'${DATAVERSEKEY}'|g' /usr/src/app/dn-backend/dn/utils.py

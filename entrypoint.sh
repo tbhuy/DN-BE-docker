30f0cb1d5bf65a91ff09c9389f9fa791fca54a33
Huy Tran <tranbahuy@gmail.com>
2021-01-28 17:55:15 +0100

    Fix source

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sed -i 's|##GRAPHDB##|'${GRAPHDB}'|g' /usr/src/app/dn-backend/dn/utils.py
+sed -i 's|#GRAPHDB#|'${GRAPHDB}'|g' /usr/src/app/dn-backend/dn/utils.py
 sed -i 's|#GRAPHDBUSER#|'${GRAPHDBUSER}'|g' /usr/src/app/dn-backend/dn/utils.py
 sed -i 's|#GRAPHDBPWD#|'${GRAPHDBPWD}'|g' /usr/src/app/dn-backend/dn/utils.py
 sed -i 's|#DATAVERSE#|'${DATAVERSE}'|g' /usr/src/app/dn-backend/dn/utils.py
 sed -i 's|#DATAVERSEEX#|'${DATAVERSEEX}'|g' /usr/src/app/dn-backend/dn/utils.py
 sed -i 's|#DATAVERSEKEY#|'${DATAVERSEKEY}'|g' /usr/src/app/dn-backend/dn/utils.py
 cd /usr/src/app/dn-backend
-python3 manage.py runserver 8000
+python3 manage.py runserver 0.0.0.0:8000
